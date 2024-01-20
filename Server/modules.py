a514e2f21b4101e4cb88be15ad2f7fc875538f6d
killgriff22 <kilgrif22033@gmail.com>
2024-01-20 14:17:40 -0500

    update bundler :)

--- Server/modules.py
+++ Server/modules.py
@@ -5,24 +5,22 @@ import time
 import hashlib
 import flask
 import json
+import zipfile
+import os
 
 
 def bundle(name, files_to_bundle=None):
-    with open("Backup/Bundler.py", "w") as f:
-        f.write(f"""import zipfile
-import os
-# Create a zip file
-z = zipfile.ZipFile('../Archives/{name}.zip', "w")
-if files_to_bundle:
-    for file in files_to_bundle:
-        z.write(name+"/"+file)
+    # Create a zip file
+    z = zipfile.ZipFile(f'Archives/{name}.zip', "w")
+    if files_to_bundle:
+        for file in files_to_bundle:
+            z.write("Backup/"+name+"/"+file)
+        z.close()
+        return
+    # Add all files in the directory
+    for root, dirs, files in os.walk(name):
+        for file in files:
+            z.write(os.path.join(root, file))
+    # Close the zip file
     z.close()
-    exit()
-# Add all files in the directory
-for root, dirs, files in os.walk(name):
-    for file in files:
-        z.write(os.path.join(root, file))
-# Close the zip file
-z.close()
-""")
-        subprocess.run(["python3", "Backup/Bundler.py"])
+    return
