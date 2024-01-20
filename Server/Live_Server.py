eae3713f61df69c4440d88a632b63dbe8ea310a4
killgriff22 <kilgrif22033@gmail.com>
2024-01-20 10:34:37 -0500

    Modify live server to not restart

--- Server/Live_Server.py
+++ Server/Live_Server.py
@@ -6,21 +6,15 @@ if len(args) >= 2:
 else:
     name = "server.py"
 try:
+    process = subprocess.Popen(['python3', name])
     while True:
-        process = subprocess.Popen(['python3', name])
         oldcontent = hash(open(name, "r").read())
         subprocess.check_output(
             ["git", "pull"])
         is_same = hash(open(name, "r").read()) == oldcontent
         if is_same:
             time.sleep(1)
-        elif process.poll() is not None:
-            print("Restarting...")
-            input(("Press enter to continue restarting..."))
-            process = subprocess.Popen(['python3', name])
         else:
-            process.kill()
-            process = subprocess.Popen(['python3', name])
             oldcontent = hash(open(name, "r").read())
 except KeyboardInterrupt:
     process.kill()
